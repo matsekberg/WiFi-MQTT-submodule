bae26530aa6452f83ad10042f28dc5749a15e7f4
Mats Ekberg <matsekberg@gmail.com>
2017-10-10 18:19:26 +0200

    Added more common stuff

--- includes.h
+++ includes.h
@@ -16,4 +16,34 @@
 #include <ArduinoOTA.h>
 
 
+#define LONG_PRESS_MS 1000
+#define SHORT_PRESS_MS 100
+#define CONFIG_WIFI_PRESS_MS 5000
+#define CONFIG_TOUCHES_COUNT 3
+#define MQTT_CHECK_MS 15000
+
+#define OTA_PASS "UPDATE_PW"
+#define OTA_PORT 8266
+
+#define BUTTON_PIN  0  // GPIO0, pin 18, D3
+#define LED_PIN     2  // GPIO2, pin 17, D4
+#define RELAY_PIN   5  // GPIO5, pin 20, D1 (SCL)
    
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long uptime = 0;
+
+
+volatile int desiredRelayState = 0;
+volatile int relayState = 0;
+volatile unsigned long millisSinceChange = 0;
+volatile int noOfConfigTouches = 0;
+
+volatile boolean sendGroupEventTopic = false;
+volatile boolean configWifi = false;
+volatile boolean sendEvent = true;
+boolean sendStatus = true;
+boolean sendPong = false;
+
+unsigned long lastMQTTCheck = -MQTT_CHECK_MS; //This will force an immediate check on init.
+bool printedWifiToSerial = false;
