09cfc5a1ffe3d9b8a01c7451215099e88d42109d
Mats Ekberg <matsekberg@gmail.com>
2017-10-13 19:24:03 +0200

    Changed to generated AP name

--- wifi-manager.cpp
+++ wifi-manager.cpp
@@ -126,7 +126,7 @@ void wifiSetup(const char* configVersion, boolean zapall) {
   // fetches ssid and pass and tries to connect
   // if it does not connect it starts an access point with the device version as name
   // and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect(configVersion, "wificonfig")) {
+  if (!wifiManager.autoConnect()) {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
     // reset and try again, or maybe put it to deep sleep
